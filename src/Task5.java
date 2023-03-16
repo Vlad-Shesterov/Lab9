4dff33e0fd1eba7946d4cb4ff0602826a4b60cb5
Влад Шестеров <vladshesterov@MacBook-Pro-Vlad.local>
2023-03-16 19:10:58 +0500

    Initial commit

---
+++ src/Task5.java
@@ -0,0 +1,15 @@
+public class Task5 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("0");
+            throw new RuntimeException("ошибка");
+        } catch (NullPointerException e) {
+            System.out.println("1");
+        }
+//Перехват ошибки(Ошибка исправлена)
+        catch (RuntimeException e) {
+            System.out.println("Перехват ошибки(Ошибка исправлена)");
+        }
+        System.out.println("2");
+    }
+}
